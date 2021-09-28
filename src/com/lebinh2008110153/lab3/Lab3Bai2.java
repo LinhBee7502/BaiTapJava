ea3d96684db94467d4bf3d2e57c7efb2255d576c
LinhBee7502 <binhleminer135@gmail.com>
2021-09-28 20:31:36 +0700

    sualaicode

--- src/com/lebinh2008110153/lab3/Lab3Bai2.java
+++ src/com/lebinh2008110153/lab3/Lab3Bai2.java
@@ -1,8 +1,13 @@
 package com.lebinh2008110153.lab3;
 
+import java.util.Scanner;
+
 public class Lab3Bai2 {
 public static void main(String[] args) {
-    for (int i=1; i<=5; i++)
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Nhap n: ");
+    int n = scanner.nextInt();
+    for (int i=1; i<=n; i++)
     {
         System.out.println("Bang cuu chuong " + i);
         for (int j = 1; j <=10; j++)
