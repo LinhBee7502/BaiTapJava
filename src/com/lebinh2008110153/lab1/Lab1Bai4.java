90c5f01a952f9a4105b54027b859bf7ee0cdb643
LinhBee7502 <binhleminer135@gmail.com>
2021-09-29 20:27:21 +0700

    suaCode

--- src/com/lebinh2008110153/lab1/Lab1Bai4.java
+++ src/com/lebinh2008110153/lab1/Lab1Bai4.java
@@ -2,7 +2,7 @@ package com.lebinh2008110153.lab1;
 
 import java.util.Scanner;
 
-public class Lab1Bai4 {
+public class Lab1Bai4 {           
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Nhap a: ");
