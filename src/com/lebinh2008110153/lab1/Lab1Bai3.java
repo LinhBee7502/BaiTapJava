49d3088a89f6c71c71b5e8f888ae28a2b561560e
LinhBee7502 <binhleminer135@gmail.com>
2021-09-29 20:30:51 +0700

    updateCode

--- src/com/lebinh2008110153/lab1/Lab1Bai3.java
+++ src/com/lebinh2008110153/lab1/Lab1Bai3.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class Lab1Bai3 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Nhap canh lap phuong: ");
-        double canh = sc.nextDouble();
+        double canh = scanner.nextDouble();
         double theTich = canh*canh*canh;
         System.out.printf("The tich cua khoi lap phuong: %.1f \n", theTich);
     }  
