eec4c4137c8107d7e869c92fcbeafcc2e9ea24a3
LinhBee7502 <binhleminer135@gmail.com>
2021-09-30 07:22:21 +0700

    upDate

--- src/com/lebinh2008110153/lab1/Lab1Bai4.java
+++ src/com/lebinh2008110153/lab1/Lab1Bai4.java
@@ -6,30 +6,15 @@ public class Lab1Bai4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Nhap a: ");
-        double a = scanner.nextDouble();
+        int a = scanner.nextInt();
         System.out.print("Nhap b: ");
-        double b = scanner.nextDouble();
+        int b = scanner.nextInt();
         System.out.print("Nhap c: ");
-        double c = scanner.nextDouble();
-        if (a==0) {
-            System.out.println("Phuong trinh co nghiem la: " + (-b/c));
-        }
-        else {
-            double delta = b*b-4*a*c;
-            if (delta < 0 ) {
-                System.out.println("Phuong trinh vo nghiem");
-            }
-            else if (delta==0) {
-                double x = -b/2*a;
-                System.out.println("Phuong trinh co nghiem kep: " + x);
-            }
-            else {
-                double x1 = (-b - Math.sqrt(delta))/2*a;
-                double x2 = (-b + Math.sqrt(delta))/2*a;
-                System.out.println("\nPhuong trinh co 2 nghiem la: ");
-                System.out.printf("\nx1= %.1f ", x1);
-                System.out.printf("x2= %.1f\n ", x2);
-            }
-        }
+        int c = scanner.nextInt();
+        double delta = (Math.pow(b,2) - (4*a*c));
+        double x1 = (-b + Math.sqrt(delta))/(2*a);
+        double x2 = (-b - Math.sqrt(delta))/(2*a);
+        System.out.printf("x1 = %.1f ", x1);
+        System.out.printf("x2 = %.1f ", x2);
     }
 }
