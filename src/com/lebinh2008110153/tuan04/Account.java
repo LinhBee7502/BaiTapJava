700dded4542a2788c3ec7f8971f4130adbc57142
LinhBee7502 <binhleminer135@gmail.com>
2021-10-07 09:08:29 +0700

    upDateBai

--- src/com/lebinh2008110153/tuan04/Account.java
+++ src/com/lebinh2008110153/tuan04/Account.java
@@ -3,16 +3,14 @@ package com.lebinh2008110153.tuan04;
 public class Account {
     int account_number;
     int account_balance;
-    public void showData() {
+    void showData() {
         System.out.println("Account Number: "+account_number);
         System.out.println("Account Balance: "+account_balance);
     }
     void deposit(int amount) {
-        account_number += amount;
-        System.out.println("Account Number: "+account_number);
+        account_number += amount;    
     }
     void withdraw(int amount) {
         account_balance -= amount;
-        System.out.println("Account Balance: "+account_balance);
     }
 }
