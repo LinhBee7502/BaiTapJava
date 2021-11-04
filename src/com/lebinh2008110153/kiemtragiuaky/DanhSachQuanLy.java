a94147c9c56a40bc6d4a4fae70bb1f236da7bf26
LinhBee7502 <binhleminer135@gmail.com>
2021-11-04 09:08:44 +0700

    cho em sua bai nay lan cuoi

--- src/com/lebinh2008110153/kiemtragiuaky/DanhSachQuanLy.java
+++ src/com/lebinh2008110153/kiemtragiuaky/DanhSachQuanLy.java
@@ -3,13 +3,13 @@ package com.lebinh2008110153.kiemtragiuaky;
 import java.util.ArrayList;
 
 public class DanhSachQuanLy {
-    private ArrayList <Object> list = new ArrayList<Object>();
+    private ArrayList <Object> list = new ArrayList<>();
     public void themNguoi(Object object) {
         list.add(object);
     }
     public void inDanhSach() {
-        for (Object ob: list) {
-            System.out.println(ob.toString());
+        for (Object obj: list) {
+            System.out.println(obj.toString());
         }
     }
 }
--- src/com/lebinh2008110153/kiemtragiuaky/QuanLyTestDrive.java
+++ src/com/lebinh2008110153/kiemtragiuaky/QuanLyTestDrive.java
@@ -3,7 +3,7 @@ package com.lebinh2008110153.kiemtragiuaky;
 public class QuanLyTestDrive {
     public static void main(String[] args) {
         
-    DanhSachQuanLy ql = new DanhSachQuanLy();
+    DanhSachQuanLy list = new DanhSachQuanLy();
     HocVien hv = new HocVien("Le Binh", "Quan Binh Thanh", 10, 10, 100);
     NhanVien nv = new NhanVien("Phan Thi Ngoc Thi", "Duc Linh", 100);
     KhachHang kh = new KhachHang("Con gai sau nay", "Quan 1", "Streamer ", 1000000000);
