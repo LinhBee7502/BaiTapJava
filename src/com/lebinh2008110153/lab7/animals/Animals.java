12d7fe768cf8084a535a611c19d4ddb690375404
LinhBee7502 <binhleminer135@gmail.com>
2021-11-10 22:25:26 +0700

    Bai lab 9

--- src/com/lebinh2008110153/lab7/animals/Animals.java
+++ src/com/lebinh2008110153/lab7/animals/Animals.java
@@ -1,11 +1,15 @@
 package com.lebinh2008110153.lab7.animals;
 
 public class Animals {
+    protected String breed;
+    protected String name;
+    protected float weight;
     protected String picture;
     protected String food;
     protected int hunger;
     protected int location;
-    protected void makeNoise() {
+    protected int age;    
+    public void makeNoise() {
         System.out.println("Tieng keu");
     }
     protected void eat() {
@@ -14,7 +18,32 @@ public class Animals {
     protected void sleep() {
         System.out.println("Ngu");
     }
-    protected void roam() {
+    public void roam() {
         System.out.println("Di chuyen");
+    }
+    public Animals() {
+        System.out.println("Tao animal.");
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+    public String getBreed() {
+        return breed;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void Introduce() {
+        makeNoise();
+        System.out.println("Tao la "+getName()+ " " + getAge() + " tuoi");
     }
 }
